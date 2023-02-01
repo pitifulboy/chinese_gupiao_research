72d5b212a91265f2d76d1468d8b3341ec7ba4cd3
pitifulboy <1020122284@qq.com>
2023-02-01 15:06:20 +0800

    日常更新

--- A_daily_review.py
+++ A_daily_review.py
@@ -8,6 +8,7 @@ from update_daily_trade_data import update_trade_data
 from update_longhubang import update_longhubang_auto
 from update_share_msg import update_share_name_from_tushare
 
+
 # 跟新数据
 update_trade_data()
 update_longhubang_auto()
